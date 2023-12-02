316f435bc406cc96a00d438a4eecfed906ab9c09
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-12-02 23:49:10 +0900

    feat: [FastMember]P/Invokeによるアンマネージな関数の呼び出しに対応

---
+++ Libs/FastMember/PInvoke/DynamicFastMethod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnembeddedResources;
+
+namespace FastMember.PInvoke
+{
+    public class DynamicFastMethod : FastMethod
+    {
+        private class ResourceSet
+        {
+            private readonly ResourceLocalizer Localizer = ResourceLocalizer.FromResXOfType<DynamicFastMethod>("FastMember");
+
+            [ResourceStringHolder(nameof(Localizer))] public Resource<string> CannotCallBeforeCompile { get; private set; }
+
+            public ResourceSet()
+            {
+                ResourceLoader.LoadAndSetAll(this);
+            }
+        }
+
+        private static readonly Lazy<ResourceSet> Resources = new Lazy<ResourceSet>();
+
+        static DynamicFastMethod()
+        {
+#if DEBUG
+            _ = Resources.Value;
+#endif
+        }
+
+        private FastMethod CompiledMethod;
+
+        public string Name { get; }
+        public bool IsCompiled { get; private set; } = false;
+        public override MethodInfo Source
+            => IsCompiled ? CompiledMethod.Source : throw new InvalidOperationException(Resources.Value.CannotCallBeforeCompile.Value);
+
+        internal DynamicFastMethod(string name)
+        {
+            Name = name;
+        }
+
+        internal void Compile(FastMethod compiledMethod)
+        {
+            CompiledMethod = compiledMethod;
+            IsCompiled = true;
+        }
+
+        public override object Invoke(object instance, object[] args) 
+            => IsCompiled ? CompiledMethod.Invoke(instance, args) : throw new InvalidOperationException(Resources.Value.CannotCallBeforeCompile.Value);
+    }
+}
---
+++ Libs/FastMember/PInvoke/DynamicLibraryCaller.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMember.PInvoke
+{
+    /// <summary>
+    /// 動的にロードされたアンマネージ ライブラリ (C や C++ などで記述された DLL) のラッパーを表します。
+    /// </summary>
+    public class DynamicLibraryCaller
+    {
+        private readonly TypeBuilder TypeBuilder;
+        private readonly List<DynamicFastMethod> DefinedFunctions = new List<DynamicFastMethod>();
+
+        public string FilePath { get; }
+        public bool IsCompiled { get; private set; } = false;
+
+        public DynamicLibraryCaller(string filePath) : this(filePath, GetDefaultTempAssemblyName(filePath))
+        {
+        }
+
+        public DynamicLibraryCaller(string filePath, AssemblyName tempAssemblyName)
+        {
+            FilePath = filePath;
+
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(tempAssemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(tempAssemblyName.Name);
+            TypeBuilder = moduleBuilder.DefineType("__DynamicCallerTempType");
+        }
+
+        private static AssemblyName GetDefaultTempAssemblyName(string filePath)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            AssemblyName assemblyName = new AssemblyName($"{fileName}.DynamicCaller.{Guid.NewGuid()}");
+            return assemblyName;
+        }
+
+        public DynamicFastMethod RegisterFunction(string functionName, Type returnType, params Type[] parameterTypes)
+        {
+            MethodBuilder methodBuilder = TypeBuilder.DefinePInvokeMethod(functionName, FilePath,
+                MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.PinvokeImpl | MethodAttributes.HideBySig, CallingConventions.Standard,
+                returnType, parameterTypes,
+                CallingConvention.StdCall, CharSet.Unicode);
+            methodBuilder.SetImplementationFlags(MethodImplAttributes.PreserveSig);
+
+            DynamicFastMethod result = new DynamicFastMethod(functionName);
+            DefinedFunctions.Add(result);
+            return result;
+        }
+
+        public void Compile()
+        {
+            Type type = TypeBuilder.CreateType();
+
+            Parallel.ForEach(DefinedFunctions, function =>
+            {
+                MethodInfo source = type.GetMethod(function.Name);
+                FastMethod method = FastMethod.Create(source);
+                function.Compile(method);
+            });
+        }
+    }
+}
