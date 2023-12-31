0eb208557504a4a9ad9e8afa9c7c9bb877df14ea
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2023-12-31 16:55:00 +0900

    feat: AtsEX拡張機能版のAtsArrayExtenderを追加

---
+++ Extensions/AtsEx.Extensions.AtsArrayExtender/ExtensionMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+using FastMember;
+using ObjectiveHarmonyPatch;
+using TypeWrapping;
+
+using AtsEx.PluginHost.Plugins;
+using AtsEx.PluginHost.Plugins.Extensions;
+
+namespace AtsEx.Extensions.AtsArrayExtender
+{
+    [PluginType(PluginType.Extension)]
+    [HideExtensionMain]
+    internal class ExtensionMain : AssemblyPluginBase, IExtension
+    {
+        private readonly static Type[] atsPluginConstructorParamTypes = new Type[]
+        {
+            typeof(UserVehicleLocationManager),
+            typeof(KeyProvider),
+            typeof(HandleSet),
+            typeof(HandleSet),
+            typeof(VehicleStateStore),
+            typeof(SectionManager),
+            typeof(MapFunctionList),
+            typeof(DoorSet),
+        };
+
+        private readonly HarmonyPatch AtsPluginConstructorPatch;
+
+        public ExtensionMain(PluginBuilder builder) : base(builder)
+        {
+            ClassMemberSet atsPluginMembers = BveHacker.BveTypes.GetClassInfoOf<AtsPlugin>();
+            FastConstructor atsPluginConstructor = atsPluginMembers.GetSourceConstructor(atsPluginConstructorParamTypes);
+
+            AtsPluginConstructorPatch = HarmonyPatch.Patch(nameof(AtsArrayExtender), atsPluginConstructor.Source, PatchType.Postfix);
+            AtsPluginConstructorPatch.Invoked += AtsPluginConstructed;
+        }
+
+        public override void Dispose()
+        {
+            AtsPluginConstructorPatch?.Dispose();
+        }
+
+        private PatchInvokationResult AtsPluginConstructed(object sender, PatchInvokedEventArgs e)
+        {
+            AtsPlugin atsPlugin = AtsPlugin.FromSource(e.Instance);
+
+            atsPlugin._PanelArray = new int[1024];
+            atsPlugin._SoundArray = new int[1024];
+            atsPlugin._OldSoundArray = new int[1024];
+
+            atsPlugin.StateStore.PanelArray = new double[1024];
+
+            return PatchInvokationResult.DoNothing(e);
+        }
+
+        public override TickResult Tick(TimeSpan elapsed)
+            => new ExtensionTickResult();
+    }
+}
---
+++ Extensions/AtsEx.Extensions.AtsArrayExtender/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("AtsArrayExtender (AtsEX 拡張機能版)")]
+[assembly: AssemblyDescription("AtsEX 拡張機能として実装した AtsArrayExtender です。ATS サウンド、パネルの配列の長さを 1024 に拡張します。")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AtsEX Project")]
+[assembly: AssemblyProduct("AtsEX")]
+[assembly: AssemblyCopyright("Copyright © 2024 automatic9045")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("ea213d83-bd73-4890-a77b-7171fc3469a9")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
