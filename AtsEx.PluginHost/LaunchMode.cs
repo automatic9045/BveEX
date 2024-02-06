4a3f78278e5e541ad50810f1bef1b83d327af814
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-02-06 22:54:36 +0900

    feat: LaunchMode列挙型の説明を文字列で取得できるように

--- AtsEx.PluginHost/LaunchMode.cs
+++ AtsEx.PluginHost/LaunchMode.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UnembeddedResources;
+
 namespace AtsEx.PluginHost
 {
     /// <summary>
@@ -20,5 +22,47 @@ namespace AtsEx.PluginHost
         /// AtsEx.Caller.InputDevice から入力デバイスプラグインとして起動されたことを表します。
         /// </summary>
         InputDevice,
+    }
+
+    public static class LaunchModeConverter
+    {
+        private class ResourceSet
+        {
+            private readonly ResourceLocalizer Localizer = ResourceLocalizer.FromResXOfType(typeof(LaunchModeConverter), "PluginHost");
+
+            [ResourceStringHolder(nameof(Localizer))] public Resource<string> Ats { get; private set; }
+            [ResourceStringHolder(nameof(Localizer))] public Resource<string> InputDevice { get; private set; }
+
+            public ResourceSet()
+            {
+                ResourceLoader.LoadAndSetAll(this);
+            }
+        }
+
+        private static readonly Lazy<ResourceSet> Resources = new Lazy<ResourceSet>();
+
+        static LaunchModeConverter()
+        {
+#if DEBUG
+            _ = Resources.Value;
+#endif
+        }
+
+        public static Resource<string> GetTypeStringResource(this LaunchMode launchMode)
+        {
+            switch (launchMode)
+            {
+                case LaunchMode.Ats:
+                    return Resources.Value.Ats;
+
+                case LaunchMode.InputDevice:
+                    return Resources.Value.InputDevice;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public static string GetTypeString(this LaunchMode launchMode) => GetTypeStringResource(launchMode).Value;
     }
 }
