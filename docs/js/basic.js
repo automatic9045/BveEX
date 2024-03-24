ac3c06dba48a0cea29a4d70c70786a7c56f17fd6
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-03-24 14:31:58 +0900

    docs: [HP]バスのランダム排出処理を統合

--- docs/js/basic.js
+++ docs/js/basic.js
@@ -187,21 +187,6 @@ function scrollTo(targetId) {
 	}
 }
 
-function getRandomBusNumbers(count = 1) {
-	let busNumbers = ["0039", "F8926", "F8926", "F8926", "F9001", "F9045", "F9045", "F9045", "F9045", "9303", "9303", "9304", "9304", "9310", "9510"];
-	const pickedBusNumbers = [];
-
-	for (let i = 0; i < count; i++) {
-		const j = Math.floor(Math.random() * busNumbers.length);
-		const pickedBusNumber = busNumbers[j];
-
-		busNumbers = busNumbers.filter(busNumber => busNumber != pickedBusNumber);
-		pickedBusNumbers.push(pickedBusNumber);
-	}
-
-	return pickedBusNumbers;
-}
-
 
 /*--Global site tag(gtag.js) - Google Analytics--*/
 
