0f9dfeb8eb0f5b98f24f3d939c01b16d2805b7bf
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2024-04-02 01:27:11 +0900

    fix: packageのフォルダ構造を修正

--- package.bat
+++ package.bat
@@ -1,8 +1,7 @@
 
-mkdir "bin\Input Devices\1.0"
-mkdir "bin\Input Devices\1.0\Resources"
-mkdir "bin\Input Devices\1.0\Extensions"
-set "dst=bin\Input Devices\"
+mkdir "bin\Input Devices\AtsEx\1.0\Resources"
+mkdir "bin\Input Devices\AtsEx\1.0\Extensions"
+set "dst=bin\Input Devices\AtsEx"
 
 xcopy "AtsEx\bin\Release\*.dll" "%dst%\1.0" /s /y
 xcopy "AtsEx\bin\Release\*.xml" "%dst%\1.0" /s /y
@@ -14,9 +13,18 @@ xcopy "AtsEx.Scripting\bin\Release\*.dll" "%dst%\1.0" /s /y
 xcopy "AtsEx.Scripting\bin\Release\*.xml" "%dst%\1.0" /s /y
 xcopy "AtsEx.Scripting\Resources\*.resx" "%dst%\1.0\Resources" /s /y
 xcopy "AtsEx.Launcher\bin\Release\*.dll" "%dst%\" /s /y
+xcopy "AtsEx.Launcher\bin\Release\*.dll.config" "%dst%\" /s /y
 xcopy "AtsEx.Launcher\bin\Release\*.xml" "%dst%\" /s /y
-xcopy "AtsEx.Caller.InputDevice\bin\Release\*.dll" "bin\" /s /y
-xcopy "AtsEx.Caller.InputDevice\bin\Release\*.xml" "bin\" /s /y
+xcopy "AtsEx.Caller.InputDevice\bin\Release\*.dll" "bin\Input Devices\" /s /y
+xcopy "AtsEx.Caller.InputDevice\bin\Release\*.xml" "bin\Input Devices\" /s /y
+
+xcopy "*.svg" "%dst%\" /y
+xcopy "*.md" "%dst%\" /y
+xcopy "LICENSE" "%dst%\" /y
+
+xcopy "*.svg" "bin\" /y
+xcopy "*.md" "bin\" /y
+xcopy "LICENSE" "bin\" /y
 
 for /d %%i in ("Extensions\*") do (
     xcopy "%%i\bin\Release\*.dll" "%dst%\1.0\Extensions" /s /y
