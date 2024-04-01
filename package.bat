1dac1da9fcc2c8694db2a57fbef8a782e42332b3
Kusaanko <39370373+kusaanko@users.noreply.github.com>
2024-04-02 01:17:48 +0900

    fix: 7zからxcopyへ切り替え

--- package.bat
+++ package.bat
@@ -1,14 +1,23 @@
-7z a bin\AtsEx.zip ..\AtsEx\AtsEx\bin\Release\*.dll -r
-7z a bin\AtsEx.zip ..\AtsEx\AtsEx\bin\Release\*.xml -r
-7z a bin\AtsEx.zip ..\AtsEx\AtsEx.PluginHost\bin\Release\*.dll -r
-7z a bin\AtsEx.zip ..\AtsEx\AtsEx.PluginHost\bin\Release\*.xml -r
-7z a bin\AtsEx.zip ..\AtsEx\AtsEx.Scripting\bin\Release\*.dll -r
-7z a bin\AtsEx.zip ..\AtsEx\AtsEx.Scripting\bin\Release\*.xml -r
-7z a bin\AtsEx.Launcher.zip ..\AtsEx\AtsEx.Launcher\bin\Release\*.dll -r
-7z a bin\AtsEx.Launcher.zip ..\AtsEx\AtsEx.Launcher\bin\Release\*.dll.config -r
-7z a bin\AtsEx.Launcher.zip ..\AtsEx\AtsEx.Launcher\bin\Release\*.xml -r
-7z a bin\AtsEx.Caller.InputDevice.zip ..\AtsEx\AtsEx.Caller.InputDevice\bin\Release\*.dll -r
-7z a bin\AtsEx.Caller.InputDevice.zip ..\AtsEx\AtsEx.Caller.InputDevice\bin\Release\*.xml -r
-7z a bin\AtsEx.SamplePlugins.zip ..\AtsEx\SamplePlugins\*\bin\Release\*.dll -r
-7z a bin\AtsEx.Extensions.zip ..\AtsEx\Extensions\*\bin\Release\*.dll -r
-7z a bin\AtsEx.Extensions.zip ..\AtsEx\Extensions\*\bin\Release\*.dll.config -r
+
+mkdir "bin\Input Devices\1.0"
+mkdir "bin\Input Devices\1.0\Resources"
+mkdir "bin\Input Devices\1.0\Extensions"
+set "dst=bin\Input Devices\"
+
+xcopy "AtsEx\bin\Release\*.dll" "%dst%\1.0" /s /y
+xcopy "AtsEx\bin\Release\*.xml" "%dst%\1.0" /s /y
+xcopy "AtsEx\Resources\*.resx" "%dst%\1.0\Resources" /s /y
+xcopy "AtsEx.PluginHost\bin\Release\*.dll" "%dst%\1.0" /s /y
+xcopy "AtsEx.PluginHost\bin\Release\*.xml" "%dst%\1.0" /s /y
+xcopy "AtsEx.PluginHost\Resources\*.resx" "%dst%\1.0\Resources" /s /y
+xcopy "AtsEx.Scripting\bin\Release\*.dll" "%dst%\1.0" /s /y
+xcopy "AtsEx.Scripting\bin\Release\*.xml" "%dst%\1.0" /s /y
+xcopy "AtsEx.Scripting\Resources\*.resx" "%dst%\1.0\Resources" /s /y
+xcopy "AtsEx.Launcher\bin\Release\*.dll" "%dst%\" /s /y
+xcopy "AtsEx.Launcher\bin\Release\*.xml" "%dst%\" /s /y
+xcopy "AtsEx.Caller.InputDevice\bin\Release\*.dll" "bin\" /s /y
+xcopy "AtsEx.Caller.InputDevice\bin\Release\*.xml" "bin\" /s /y
+
+for /d %%i in ("Extensions\*") do (
+    xcopy "%%i\bin\Release\*.dll" "%dst%\1.0\Extensions" /s /y
+)
