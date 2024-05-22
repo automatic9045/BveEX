6840dfb5a3dcacf27ca182102122b24fd3b68d17
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-05-22 21:52:48 +0900

    refactor: [Launcher]エラーダイアログを表示する処理を整理

---
+++ AtsEx.Launcher/ErrorDialog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtsEx.Launcher
+{
+    internal static class ErrorDialog
+    {
+        public static void Show(int id, string message, string approach)
+        {
+            Diagnostics.ErrorDialog.Show($"エラーコード L-{id}\n{message}", Resources.Name, approach, $"https://www.okaoka-depot.com/AtsEX.Docs/support/errors/#L-{id}");
+        }
+    }
+}
--- AtsEx.Launcher/Hosting/TargetBveFinder.cs
+++ AtsEx.Launcher/Hosting/TargetBveFinder.cs
@@ -6,8 +6,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-using AtsEx.Diagnostics;
-
 namespace AtsEx.Launcher.Hosting
 {
     internal class TargetBveFinder
@@ -29,19 +27,14 @@ namespace AtsEx.Launcher.Hosting
         {
             if (TargetAssembly is null)
             {
-                ShowErrorDialog(1, "BVE 本体の読込に失敗しました。Assembly.GetEntryAssembly が null を返しました。",
+                ErrorDialog.Show(1, "BVE 本体の読込に失敗しました。Assembly.GetEntryAssembly が null を返しました。",
                     "想定されない方法で AtsEX を起動しています。BVE 本体と異なるプロセス上で AtsEX を実行していませんか?");
+                throw new NotSupportedException();
             }
             else if (!TargetAssembly.GetTypes().Any(t => t.Namespace == "Mackoy.Bvets"))
             {
-                ShowErrorDialog(2, "同一プロセス上に BVE 本体が見つかりませんでした。",
+                ErrorDialog.Show(2, "同一プロセス上に BVE 本体が見つかりませんでした。",
                     "想定されない方法で AtsEX を起動しています。BVE 本体と異なるプロセス上で AtsEX を実行していませんか?");
-            }
-
-
-            void ShowErrorDialog(int id, string message, string approach)
-            {
-                ErrorDialog.Show($"エラーコード L-{id}\n{message}", "AtsEX Launcher", approach, $"https://www.okaoka-depot.com/AtsEX.Docs/support/errors/#L-{id}");
                 throw new NotSupportedException();
             }
         }
