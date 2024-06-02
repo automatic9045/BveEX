5c0ccd02387c860e0a4f4c6261d4c5f6b0deca7c
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-06-03 00:26:11 +0900

    feat: [マップステートメント]デバッグ用のダイアログ表示構文を実装

--- Extensions/AtsEx.CoreExtensions/MapStatements/Builtin/BuiltinProcess.cs
+++ Extensions/AtsEx.CoreExtensions/MapStatements/Builtin/BuiltinProcess.cs
@@ -5,12 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 using AtsEx.Extensions.MapStatements.Builtin.Preprocess;
+using AtsEx.Extensions.MapStatements.Builtin.Statements;
 
 namespace AtsEx.Extensions.MapStatements.Builtin
 {
     internal class BuiltinProcess
     {
         private readonly IfBlock IfBlock = new IfBlock();
+
+        private readonly List<IParser> StatementParsers = new List<IParser>()
+        {
+            new Dialog(),
+        };
 
         public bool IgnoreStatement => IfBlock.IgnoreStatement;
 
@@ -20,6 +26,18 @@ namespace AtsEx.Extensions.MapStatements.Builtin
             {
                 IfBlock.Parse(statement);
                 return true;
+            }
+
+            if (!IgnoreStatement)
+            {
+                foreach (IParser parser in StatementParsers)
+                {
+                    if (parser.CanParse(statement))
+                    {
+                        parser.Parse(statement);
+                        break;
+                    }
+                }
             }
 
             return false;
