4d43fc2d9d1141468d78aaa65640d5f1f6242201
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-06-26 00:39:49 +0900

    sample: 独自マップ構文サンプルをマップステートメント機能対応に書き換え

---
+++ SamplePlugins/MapPlugins/MapStatementTest/Headers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AtsEx.PluginHost;
+using AtsEx.PluginHost.MapStatements;
+
+namespace AtsEx.Samples.MapPlugins.MapStatementTest
+{
+    internal static class Headers
+    {
+        private static readonly string AssemblyFileName = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+
+        private static readonly Namespace Namespace = Namespace.GetUserNamespace("Automatic9045").Child("Alert");
+        private static readonly Identifier HeaderAlert = new Identifier(Namespace, "HeaderAlert");
+
+        public static void Load(IHeaderSet mapHeaders)
+        {
+            IReadOnlyList<IHeader> headers = mapHeaders.GetAll(HeaderAlert);
+            for (int i = 0; i < headers.Count; i++)
+            {
+                IHeader header = headers[i];
+                MessageBox.Show($"ヘッダー {header.Name.FullName} が読み込まれました ({i}):\n\n{header.Argument}");
+            }
+
+            if (!headers.Any())
+            {
+                throw new BveFileLoadException($"ヘッダー {HeaderAlert.FullName} が見つかりませんでした。", AssemblyFileName);
+            }
+        }
+    }
+}
---
+++ SamplePlugins/MapPlugins/MapStatementTest/Statements.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using BveTypes.ClassWrappers;
+
+using AtsEx.Extensions.MapStatements;
+using AtsEx.PluginHost;
+
+namespace AtsEx.Samples.MapPlugins.MapStatementTest
+{
+    internal static class Statements
+    {
+        public static void Load(IStatementSet mapStatements)
+        {
+            ClauseFilter[] clauseFilters = new ClauseFilter[]
+            {
+                new ClauseFilter(nameof(MapStatementTest), ClauseType.Element),
+                new ClauseFilter("Greetings", ClauseType.Element),
+                new ClauseFilter("Put", ClauseType.Function),
+            };
+            IEnumerable<Statement> statements = mapStatements.FindUserStatements("Automatic9045", clauseFilters);
+
+            foreach (Statement statement in statements)
+            {
+                string fileName = statement.Source.FileName;
+                double location = statement.Source.Location;
+
+                IList<MapStatementClause> clauses = statement.Source.Clauses;
+
+                MapStatementClause element = clauses[clauses.Count - 2];
+                if (2 < element.Keys.Count) throw new BveFileLoadException($"キーの長さが不正です。", fileName, element.LineIndex, element.CharIndex);
+
+                MapStatementClause function = statement.Source.Clauses[statement.Source.Clauses.Count - 1];
+                if (2 < function.Args.Count) throw new BveFileLoadException($"引数の長さが不正です。", fileName, function.LineIndex, function.CharIndex);
+
+                MessageBox.Show($"定義位置: '{Path.GetFileName(fileName)}', 行 {function.LineIndex}\n距離程: {location} m\n\n" +
+                    $"キー 1: {(0 < element.Keys.Count ? ToString(element.Keys[0]) : "(なし)")}\n" +
+                    $"キー 2: {(1 < element.Keys.Count ? ToString(element.Keys[1]) : "(なし)")}\n" +
+                    $"引数 1: {(0 < function.Args.Count ? ToString(function.Args[0]) : "(なし)")}\n" +
+                    $"引数 2: {(1 < function.Args.Count ? ToString(function.Args[1]) : "(なし)")}");
+
+
+                string ToString(object value) => value is null ? "(null)" : value is string ? $"\"{value}\"" : value.ToString();
+            }
+        }
+    }
+}
