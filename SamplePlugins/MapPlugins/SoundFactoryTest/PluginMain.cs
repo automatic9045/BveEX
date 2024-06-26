5d90ca2ed3afd9d0a6f7985a02acde2ce02e0444
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-06-26 21:31:44 +0900

    sample: SoundFactoryを利用して音声を読み込み、再生するサンプルを追加

---
+++ SamplePlugins/MapPlugins/SoundFactoryTest/PluginMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+
+using AtsEx.PluginHost;
+using AtsEx.PluginHost.Plugins;
+
+using AtsEx.Extensions.SoundFactory;
+
+namespace AtsEx.Samples.MapPlugins.SoundFactoryTest
+{
+    [Plugin(PluginType.MapPlugin)]
+    public class PluginMain : AssemblyPluginBase
+    {
+        private Sound SampleSound;
+
+        public PluginMain(PluginBuilder builder) : base(builder)
+        {
+            BveHacker.ScenarioCreated += OnScenarioCreated;
+        }
+
+        public override void Dispose()
+        {
+            BveHacker.ScenarioCreated -= OnScenarioCreated;
+            SampleSound.Dispose();
+        }
+
+        private void OnScenarioCreated(ScenarioCreatedEventArgs e)
+        {
+            ISoundFactory soundFactory = Extensions.GetExtension<ISoundFactory>();
+
+            string path = Path.Combine(Path.GetDirectoryName(Location), "Sample.wav");
+            SampleSound = soundFactory.LoadFrom(path, 1, Sound.SoundPosition.Cab);
+
+            SampleSound.Play(1, 1, 0);
+        }
+
+        public override TickResult Tick(TimeSpan elapsed)
+        {
+            return new MapPluginTickResult();
+        }
+    }
+}
