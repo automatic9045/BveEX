d9606e831ad13e8130c9cc67ac14d1d725b994fe
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2024-10-27 13:23:37 +0900

    chore: [Workflow]サンプルシナリオ非同梱のアーティファクトも作成するように

--- package.bat
+++ package.bat
@@ -1,18 +1,20 @@
-xcopy "_out\AtsEx\" "bin\Input Devices\AtsEx\" /e /s /y
-move "bin\Input Devices\AtsEx\Debug" "bin\Input Devices\AtsEx\1.0"
-xcopy "_out\Scenarios\" "bin\Scenarios\" /e /s /y
+set dest=%1
 
-xcopy "AtsEx.Caller.InputDevice\bin\Release\*.dll" "bin\Input Devices\" /s /y
-xcopy "AtsEx.Caller.InputDevice\bin\Release\*.xml" "bin\Input Devices\" /s /y
+xcopy "_out\AtsEx\" "%dest%\Input Devices\AtsEx\" /e /s /y
+move "%dest%\Input Devices\AtsEx\Debug" "%dest%\Input Devices\AtsEx\1.0"
+if "%2"=="/s" xcopy "_out\Scenarios\" "%dest%\Scenarios\" /e /s /y
 
-xcopy "*.svg" "bin\Input Devices\AtsEx\" /y
-xcopy "*.md" "bin\Input Devices\AtsEx\" /y
-xcopy "LICENSE" "bin\Input Devices\AtsEx\" /y
+xcopy "AtsEx.Caller.InputDevice\bin\Release\*.dll" "%dest%\Input Devices\" /s /y
+xcopy "AtsEx.Caller.InputDevice\bin\Release\*.xml" "%dest%\Input Devices\" /s /y
 
-xcopy "*.svg" "bin\" /y
-xcopy "*.md" "bin\" /y
-xcopy "LICENSE" "bin\" /y
+xcopy "*.svg" "%dest%\Input Devices\AtsEx\" /y
+xcopy "*.md" "%dest%\Input Devices\AtsEx\" /y
+xcopy "LICENSE" "%dest%\Input Devices\AtsEx\" /y
 
-cd "bin\"
+xcopy "*.svg" "%dest%\" /y
+xcopy "*.md" "%dest%\" /y
+xcopy "LICENSE" "%dest%\" /y
+
+cd "%dest%\"
 del "*.pdb" /s
 del "*.tmp" /s
