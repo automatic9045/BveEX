9bc779b6263bf623227c226da1fda8bd6fb06d1a
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-01-02 16:05:42 +0900

    fix: AtsEX向け・BveEX向けのコードが混在していると無限ループする不具合を修正

--- BveEx/Launching/LaunchModeManager.cs
+++ BveEx/Launching/LaunchModeManager.cs
@@ -6,19 +6,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UnembeddedResources;
+
 using BveEx.PluginHost;
 
 namespace BveEx.Launching
 {
     internal static class LaunchModeManager
     {
+        private class ResourceSet
+        {
+            private readonly ResourceLocalizer Localizer = ResourceLocalizer.FromResXOfType(typeof(LaunchModeManager), "Core");
+
+            [ResourceStringHolder(nameof(Localizer))] public Resource<string> ConflictedMessage { get; private set; }
+            [ResourceStringHolder(nameof(Localizer))] public Resource<string> ConflictedApproach { get; private set; }
+
+            public ResourceSet()
+            {
+                ResourceLoader.LoadAndSetAll(this);
+            }
+        }
+
+        private static readonly Lazy<ResourceSet> Resources = new Lazy<ResourceSet>();
+
+        static LaunchModeManager()
+        {
+#if DEBUG
+            _ = Resources.Value;
+#endif
+        }
+
         public static void RestartAsLegacyMode(string scenarioPath)
         {
+            string[] commandLineArgs = Environment.GetCommandLineArgs();
+            if (commandLineArgs.Contains("/bveex-modechanged", StringComparer.OrdinalIgnoreCase))
+            {
+                ErrorDialog.Show(5, Resources.Value.ConflictedMessage.Value, Resources.Value.ConflictedApproach.Value);
+                throw new InvalidOperationException(Resources.Value.ConflictedMessage.Value);
+            }
+
             string legacyFilePath = Path.Combine(Path.GetDirectoryName(App.Instance.BveExLauncherAssembly.Location), ".LEGACY");
             File.Create(legacyFilePath).Close();
             File.SetAttributes(legacyFilePath, FileAttributes.Hidden);
 
-            Process.Start(App.Instance.BveAssembly.Location, scenarioPath is null ? string.Empty : $"\"{scenarioPath}\"");
+            Process.Start(App.Instance.BveAssembly.Location, scenarioPath is null ? string.Empty : $"\"{scenarioPath}\" /bveex-modechanged");
             Environment.Exit(0);
         }
     }
