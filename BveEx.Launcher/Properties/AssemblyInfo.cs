f817e111d041ace3e2f55ba45c0af482c804f284
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-01-03 00:44:50 +0900

    chore: バージョンを2.0.50103.1に

--- BveEx.Launcher/Properties/AssemblyInfo.cs
+++ BveEx.Launcher/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-using BveEx.Launcher;
-
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
@@ -34,4 +32,4 @@ using BveEx.Launcher;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.41222.1")]
+[assembly: AssemblyVersion("2.0.50103.1")]
