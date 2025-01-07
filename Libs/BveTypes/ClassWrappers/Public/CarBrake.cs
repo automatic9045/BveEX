49e4d3522b13cc212df106ab1f31f80305558260
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-01-07 22:19:56 +0900

    feat: [クラスラッパー]CarBrakeにITickableを実装

--- Libs/BveTypes/ClassWrappers/Public/CarBrake.cs
+++ Libs/BveTypes/ClassWrappers/Public/CarBrake.cs
@@ -12,7 +12,7 @@ namespace BveTypes.ClassWrappers
     /// <summary>
     /// 車両のブレーキを表します。
     /// </summary>
-    public class CarBrake : ClassWrapperBase
+    public class CarBrake : ClassWrapperBase, ITickable
     {
         [InitializeClassWrapper]
         private static void Initialize(BveTypeSet bveTypes)
@@ -22,6 +22,9 @@ namespace BveTypes.ClassWrappers
             BcValveGetMethod = members.GetSourcePropertyGetterOf(nameof(BcValve));
             BasicBrakeGetMethod = members.GetSourcePropertyGetterOf(nameof(BasicBrake));
             BrakeReAdhesionGetMethod = members.GetSourcePropertyGetterOf(nameof(BrakeReAdhesion));
+
+            InitializeMethod = members.GetSourceMethodOf(nameof(Initialize));
+            TickMethod = members.GetSourceMethodOf(nameof(Tick));
         }
 
         /// <summary>
@@ -57,5 +60,26 @@ namespace BveTypes.ClassWrappers
         /// 基礎ブレーキ装置の滑走再粘着制御機構を取得します。
         /// </summary>
         public ReAdhesionControl BrakeReAdhesion => ReAdhesionControl.FromSource(BrakeReAdhesionGetMethod.Invoke(Src, null));
+
+        private static FastMethod InitializeMethod;
+        /// <inheritdoc/>
+        public void Initialize()
+            => InitializeMethod.Invoke(Src, null);
+
+        private static FastMethod TickMethod;
+        /// <inheritdoc/>
+        public void Tick(double elapsedSeconds)
+            => TickMethod.Invoke(Src, new object[] { elapsedSeconds });
+
+        /// <summary>
+        /// 毎フレーム呼び出されます。
+        /// </summary>
+        /// <remarks>
+        /// このメソッドはオリジナルではないため、<see cref="ClassMemberSet.GetSourceMethodOf(string, Type[])"/> メソッドから参照することはできません。<br/>
+        /// このメソッドのオリジナルバージョンは <see cref="Tick(double)"/> です。
+        /// </remarks>
+        /// <param name="elapsed">前フレームからの経過時間。</param>
+        /// <seealso cref="Tick(double)"/>
+        public void Tick(TimeSpan elapsed) => Tick(elapsed.TotalSeconds);
     }
 }
