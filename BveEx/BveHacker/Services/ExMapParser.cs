af8d8466deadd1aa737ea96fe1ae478d3ca98dd1
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-02-02 01:13:56 +0900

    feat: マップで使用できる演算子を追加

---
+++ BveEx/BveHacker/Services/ExMapParser.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using BveTypes.ClassWrappers;
+using Irony.Parsing;
+
+namespace BveEx.BveHackerServices
+{
+    internal class ExMapParser
+    {
+        private readonly FieldInfo DataField;
+
+        public ExMapParser()
+        {
+            DataField = typeof(BnfExpression)
+                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField)
+                .First(field => field.FieldType.IsSubclassOf(typeof(List<BnfTermList>)));
+        }
+
+        private IEnumerable<BnfTerm> GetExpressionData(BnfExpression instance)
+        {
+            List<BnfTermList> terms = DataField.GetValue(instance) as List<BnfTermList>;
+            return terms.SelectMany(x => x);
+        }
+
+        public void ConstructGrammar(Grammar grammar)
+        {
+            grammar.RegisterOperators(5, "*", "/", "%");
+            grammar.RegisterOperators(4, "+", "-");
+            grammar.RegisterOperators(3, "<", ">", "<=", ">=");
+            grammar.RegisterOperators(2, "==", "!=");
+            grammar.RegisterOperators(1, "&&");
+            grammar.RegisterOperators(0, "||");
+
+
+            NonTerminal statementList = grammar.Root;
+
+            IEnumerable<BnfTerm> statementListRules = GetExpressionData(statementList.Rule);
+            NonTerminal statement = statementListRules.First(x => x.Name == "Statement") as NonTerminal;
+
+            IEnumerable<BnfTerm> statementRules = GetExpressionData(statement.Rule);
+            NonTerminal expression = statementRules.First(x => x.Name == "Expression") as NonTerminal;
+
+            IEnumerable<BnfTerm> expressionRules = GetExpressionData(expression.Rule);
+            NonTerminal operatorExpression = expressionRules.First(x => x.Name == "OperatorExpression") as NonTerminal;
+            NonTerminal unaryExpression = expressionRules.First(x => x.Name == "UnaryExpression") as NonTerminal;
+
+            IEnumerable<BnfTerm> operatorExpressionRules = GetExpressionData(operatorExpression.Rule);
+            NonTerminal @operator = operatorExpressionRules.First(x => x.Name == "Operator") as NonTerminal;
+
+            IEnumerable<BnfTerm> unaryExpressionRules = GetExpressionData(unaryExpression.Rule);
+            NonTerminal unaryOperator = unaryExpressionRules.First(x => x.Name == "UnaryOperator") as NonTerminal;
+
+
+            @operator.Rule |= grammar.ToTerm("<") | ">" | "<=" | ">=" | "==" | "!=" | "&&" | "||";
+            unaryOperator.Rule |= grammar.ToTerm("!");
+        }
+
+        private bool ToBool(object value)
+        {
+            switch (value)
+            {
+                case string stringValue:
+                    return !(stringValue is null);
+
+                case double doubleValue:
+                    return doubleValue != 0;
+
+                case int intValue:
+                    return intValue != 0;
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public void ParseNode(MapParser parser, ParseTreeNode node)
+        {
+            try
+            {
+                switch (node.Term.Name)
+                {
+                    case "IncludeStatement":
+                        parser.Include(node);
+                        break;
+
+                    case "VariableDeclarator":
+                        parser.SetUserVariable(node);
+                        break;
+
+                    case "SYNTAX_ERROR":
+                        break;
+
+                    default:
+                        parser.SetLocation(node);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                ParseTreeNode item = node;
+
+                int lineIndex = 0;
+                int columnIndex = 0;
+                while (true)
+                {
+                    if (item.Token is null)
+                    {
+                        if (item.ChildNodes.Count <= 0) break;
+                    }
+                    else
+                    {
+                        lineIndex = item.Token.Location.Line + 2;
+                        columnIndex = item.Token.Location.Column + 1;
+                        break;
+                    }
+
+                    item = item.ChildNodes[0];
+                }
+
+                LoadError error = new LoadError(ex.Message, parser.FilePath, lineIndex, columnIndex);
+                parser.Parent.ThrowError(error);
+            }
+        }
+
+        public bool TryGetSystemVariable(MapParser parser, string key, out object result)
+        {
+            switch (key)
+            {
+                case "ex_relativedir":
+                    Uri rootUri = new Uri(Path.GetDirectoryName(((MapLoader)parser.Parent).FilePath) + Path.DirectorySeparatorChar);
+                    Uri subUri = new Uri(Path.GetDirectoryName(parser.FilePath) + Path.DirectorySeparatorChar);
+
+                    Uri relativeUri = rootUri.MakeRelativeUri(subUri);
+                    string relativePath = relativeUri.ToString().Replace('/', Path.DirectorySeparatorChar);
+
+                    result = relativePath;
+                    return true;
+
+                default:
+                    result = default;
+                    return false;
+            }
+        }
+
+        public bool TryCalculateSign(object value, string operatorName, out object result)
+        {
+            switch (operatorName)
+            {
+                case "!":
+                    result = !ToBool(value) ? 1 : 0;
+                    return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        public bool TryCalculateOperator(object left, object right, string operatorName, out object result)
+        {
+            if ((left is int || left is double) && (right is int || right is double))
+            {
+                double leftDouble = Convert.ToDouble(left, CultureInfo.InvariantCulture);
+                double rightDouble = Convert.ToDouble(right, CultureInfo.InvariantCulture);
+
+                switch (operatorName)
+                {
+                    case "==":
+                        result = leftDouble == rightDouble ? 1 : 0;
+                        return true;
+                    case "!=":
+                        result = leftDouble != rightDouble ? 1 : 0;
+                        return true;
+                    case "&&":
+                        result = ToBool(leftDouble) && ToBool(rightDouble) ? 1 : 0;
+                        return true;
+                    case "||":
+                        result = ToBool(leftDouble) || ToBool(rightDouble) ? 1 : 0;
+                        return true;
+                    case "<":
+                        result = leftDouble < rightDouble ? 1 : 0;
+                        return true;
+                    case ">":
+                        result = leftDouble > rightDouble ? 1 : 0;
+                        return true;
+                    case "<=":
+                        result = leftDouble <= rightDouble ? 1 : 0;
+                        return true;
+                    case ">=":
+                        result = leftDouble >= rightDouble ? 1 : 0;
+                        return true;
+                }
+            }
+            else
+            {
+                switch (operatorName)
+                {
+                    case "==":
+                        result = left == right ? 1 : 0;
+                        return true;
+                    case "!=":
+                        result = left != right ? 1 : 0;
+                        return true;
+                    case "&&":
+                        result = ToBool(left) && ToBool(right) ? 1 : 0;
+                        return true;
+                    case "||":
+                        result = ToBool(left) || ToBool(right) ? 1 : 0;
+                        return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
--- BveEx/BveHacker/Services/MapLoaderHacker.cs
+++ BveEx/BveHacker/Services/MapLoaderHacker.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,7 @@ using BveTypes;
 using BveTypes.ClassWrappers;
 using BveTypes.ClassWrappers.Extensions;
 using FastMember;
+using Irony;
 using Irony.Parsing;
 using ObjectiveHarmonyPatch;
 using TypeWrapping;
@@ -20,27 +20,32 @@ namespace BveEx.BveHackerServices
 {
     internal class MapLoaderHacker : IDisposable
     {
-        private readonly HarmonyPatch ConstructorPatch;
+        private readonly HarmonyPatch MapLoaderCtorPatch;
         private readonly HarmonyPatch ParseStatementPatch;
         private readonly HarmonyPatch IncludePatch;
+        private readonly HarmonyPatch ParsePatch;
         private readonly HarmonyPatch GetSystemVariablePatch;
+        private readonly HarmonyPatch CalculateSignPatch;
+        private readonly HarmonyPatch CalculateOperatorPatch;
+        private readonly HarmonyPatch MapGrammarCtorPatch;
 
         public MapLoader MapLoader { get; private set; } = null;
+        private readonly ExMapParser ExParser = new ExMapParser();
 
         public MapLoaderHacker(BveTypeSet bveTypes)
         {
             ClassMemberSet mapLoaderMembers = bveTypes.GetClassInfoOf<MapLoader>();
 
-            FastConstructor constructor = mapLoaderMembers.GetSourceConstructor();
-            ConstructorPatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), constructor.Source, PatchType.Postfix);
-            ConstructorPatch.Invoked += (sender, e) =>
+            FastConstructor mapLoaderCtor = mapLoaderMembers.GetSourceConstructor();
+            MapLoaderCtorPatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), mapLoaderCtor.Source, PatchType.Postfix);
+            MapLoaderCtorPatch.Invoked += (sender, e) =>
             {
                 MapLoader = MapLoader.FromSource(e.Instance);
 
                 return PatchInvokationResult.DoNothing(e);
             };
 
-            FastMethod parseStatementMethod = mapLoaderMembers.GetSourceMethodOf(nameof(MapLoader.ParseStatement));
+            FastMethod parseStatementMethod = mapLoaderMembers.GetSourceMethodOf(nameof(BveTypes.ClassWrappers.MapLoader.ParseStatement));
             ParseStatementPatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), parseStatementMethod.Source, PatchType.Prefix);
             ParseStatementPatch.Invoked += (sender, e) =>
             {
@@ -53,6 +58,7 @@ namespace BveEx.BveHackerServices
 
                 return PatchInvokationResult.DoNothing(e);
             };
+
 
             ClassMemberSet mapParserMembers = bveTypes.GetClassInfoOf<MapParser>();
 
@@ -72,7 +78,38 @@ namespace BveEx.BveHackerServices
                 else if (!HeaderParser.IsNoMapPluginHeader(argText))
                 {
                     object result = instance.GetValue(node.ChildNodes[1]);
-                    MapLoader.Include(Convert.ToString(result));
+                    instance.Parent.Include(Convert.ToString(result));
+                }
+
+                return new PatchInvokationResult(SkipModes.SkipOriginal);
+            };
+
+            FastMethod parseMethod = mapParserMembers.GetSourceMethodOf(nameof(MapParser.Parse));
+            ParsePatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), parseMethod.Source, PatchType.Prefix);
+            ParsePatch.Invoked += (sender, e) =>
+            {
+                MapParser instance = MapParser.FromSource(e.Instance);
+
+                string sourceText = (string)e.Args[0];
+                string filePath = (string)e.Args[1];
+
+                instance.FilePath = filePath;
+                MapGrammar grammar = new MapGrammar();
+                Parser parser = new Parser(grammar.Src);
+                ParseTree parseTree = parser.Parse(sourceText);
+
+                foreach (LogMessage log in parseTree.ParserMessages)
+                {
+                    LoadError error = new LoadError(log.Message, filePath, log.Location.Line + 2, log.Location.Column + 1);
+                    instance.Parent.ThrowError(error);
+                }
+
+                if (parseTree.Root != null)
+                {
+                    foreach (ParseTreeNode node in parseTree.Root.ChildNodes)
+                    {
+                        ExParser.ParseNode(instance, node);
+                    }
                 }
 
                 return new PatchInvokationResult(SkipModes.SkipOriginal);
@@ -82,37 +119,74 @@ namespace BveEx.BveHackerServices
             GetSystemVariablePatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), getSystemVariableMethod.Source, PatchType.Prefix);
             GetSystemVariablePatch.Invoked += (sender, e) =>
             {
+                MapParser instance = MapParser.FromSource(e.Instance);
+
                 WrappedList<MapStatementClause> clauses = WrappedList<MapStatementClause>.FromSource((IList)e.Args[0]);
                 ParseTreeNode node = (ParseTreeNode)e.Args[1];
 
                 if (0 < clauses.Count) return PatchInvokationResult.DoNothing(e);
 
                 string key = ((string)node.Token.Value).ToLowerInvariant();
-                switch (key)
-                {
-                    case "ex_relativedir":
-                        MapParser instance = MapParser.FromSource(e.Instance);
 
-                        Uri rootUri = new Uri(Path.GetDirectoryName(MapLoader.FilePath) + Path.DirectorySeparatorChar);
-                        Uri subUri = new Uri(Path.GetDirectoryName(instance.FilePath) + Path.DirectorySeparatorChar);
+                bool parsed = ExParser.TryGetSystemVariable(instance, key, out object result);
+                return parsed ? new PatchInvokationResult(result, SkipModes.SkipOriginal) : PatchInvokationResult.DoNothing(e);
+            };
 
-                        Uri relativeUri = rootUri.MakeRelativeUri(subUri);
-                        string relativePath = relativeUri.ToString().Replace('/', Path.DirectorySeparatorChar);
+            FastMethod calculateSignMethod = mapParserMembers.GetSourceMethodOf(nameof(MapParser.CalculateSign));
+            CalculateSignPatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), calculateSignMethod.Source, PatchType.Prefix);
+            CalculateSignPatch.Invoked += (sender, e) =>
+            {
+                MapParser instance = MapParser.FromSource(e.Instance);
 
-                        return new PatchInvokationResult(relativePath, SkipModes.SkipOriginal);
+                ParseTreeNode node = (ParseTreeNode)e.Args[0];
 
-                    default:
-                        return PatchInvokationResult.DoNothing(e);
-                }
+                object value = instance.GetValue(node.ChildNodes[1]);
+                string operatorName = node.ChildNodes[0].Term.Name;
+
+                bool parsed = ExParser.TryCalculateSign(value, operatorName, out object result);
+                return parsed ? new PatchInvokationResult(result, SkipModes.SkipOriginal) : PatchInvokationResult.DoNothing(e);
+            };
+
+            FastMethod calculateOperatorMethod = mapParserMembers.GetSourceMethodOf(nameof(MapParser.CalculateOperator));
+            CalculateOperatorPatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), calculateOperatorMethod.Source, PatchType.Prefix);
+            CalculateOperatorPatch.Invoked += (sender, e) =>
+            {
+                MapParser instance = MapParser.FromSource(e.Instance);
+
+                ParseTreeNode node = (ParseTreeNode)e.Args[0];
+
+                object left = instance.GetValue(node.ChildNodes[0]);
+                object right = instance.GetValue(node.ChildNodes[2]);
+                string operatorName = node.ChildNodes[1].Term.Name;
+
+                bool parsed = ExParser.TryCalculateOperator(left, right, operatorName, out object result);
+                return parsed ? new PatchInvokationResult(result, SkipModes.SkipOriginal) : PatchInvokationResult.DoNothing(e);
+            };
+
+
+            ClassMemberSet mapGrammarMembers = bveTypes.GetClassInfoOf<MapGrammar>();
+
+            FastConstructor mapGrammarCtor = mapGrammarMembers.GetSourceConstructor();
+            MapGrammarCtorPatch = HarmonyPatch.Patch(nameof(MapLoaderHacker), mapGrammarCtor.Source, PatchType.Postfix);
+            MapGrammarCtorPatch.Invoked += (sender, e) =>
+            {
+                MapGrammar instance = MapGrammar.FromSource(e.Instance);
+                ExParser.ConstructGrammar(instance.Src);
+
+                return PatchInvokationResult.DoNothing(e);
             };
         }
 
         public void Dispose()
         {
-            ConstructorPatch.Dispose();
+            MapLoaderCtorPatch.Dispose();
             ParseStatementPatch.Dispose();
             IncludePatch.Dispose();
+            ParsePatch.Dispose();
             GetSystemVariablePatch.Dispose();
+            CalculateSignPatch.Dispose();
+            CalculateOperatorPatch.Dispose();
+            MapGrammarCtorPatch.Dispose();
         }
 
         public void Clear()
