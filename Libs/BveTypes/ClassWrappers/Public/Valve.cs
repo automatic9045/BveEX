ad2afbfde9647b54e798221afc6f0530a2b1aa19
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-02-03 00:35:26 +0900

    feat: [クラスラッパー]Valve.TickメソッドでTimeSpanを使用可能に

--- Libs/BveTypes/ClassWrappers/Public/Valve.cs
+++ Libs/BveTypes/ClassWrappers/Public/Valve.cs
@@ -122,12 +122,21 @@ namespace BveTypes.ClassWrappers
 
         private static FastMethod InitializeMethod;
         /// <inheritdoc/>
-        public void Initialize()
-            => InitializeMethod.Invoke(Src, null);
+        public void Initialize() => InitializeMethod.Invoke(Src, null);
 
         private static FastMethod TickMethod;
         /// <inheritdoc/>
-        public void Tick(double elapsedSeconds)
-            => TickMethod.Invoke(Src, new object[] { elapsedSeconds });
+        public void Tick(double elapsedSeconds) => TickMethod.Invoke(Src, new object[] { elapsedSeconds });
+
+        /// <summary>
+        /// 毎フレーム呼び出されます。
+        /// </summary>
+        /// <remarks>
+        /// このメソッドはオリジナルではないため、<see cref="ClassMemberSet.GetSourceMethodOf(string, Type[])"/> メソッドから参照することはできません。<br/>
+        /// このメソッドのオリジナルバージョンは <see cref="Tick(double)"/> です。
+        /// </remarks>
+        /// <param name="elapsed">前フレームからの経過時間。</param>
+        /// <seealso cref="Tick(double)"/>
+        public void Tick(TimeSpan elapsed) => Tick(elapsed.TotalSeconds);
     }
 }
