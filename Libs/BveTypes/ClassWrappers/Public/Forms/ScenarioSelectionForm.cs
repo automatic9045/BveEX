66aec7be03b16049da8d1ce9ffea9d4ce068d046
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-03-14 00:22:40 +0900

    feat: [クラスラッパー]「シナリオの選択」フォームの設定保存処理を操作可能に

--- Libs/BveTypes/ClassWrappers/Public/Forms/ScenarioSelectionForm.cs
+++ Libs/BveTypes/ClassWrappers/Public/Forms/ScenarioSelectionForm.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using FastMember;
+using TypeWrapping;
+
 namespace BveTypes.ClassWrappers
 {
     /// <summary>
@@ -11,6 +14,14 @@ namespace BveTypes.ClassWrappers
     /// </summary>
     public class ScenarioSelectionForm : ClassWrapperBase
     {
+        [InitializeClassWrapper]
+        private static void Initialize(BveTypeSet bveTypes)
+        {
+            ClassMemberSet members = bveTypes.GetClassInfoOf<ScenarioSelectionForm>();
+
+            SaveSettingsMethod = members.GetSourceMethodOf(nameof(SaveSettings));
+        }
+
         /// <summary>
         /// オリジナル オブジェクトから <see cref="ScenarioSelectionForm"/> クラスの新しいインスタンスを初期化します。
         /// </summary>
@@ -26,5 +37,11 @@ namespace BveTypes.ClassWrappers
         /// <returns>オリジナル オブジェクトをラップした <see cref="ScenarioSelectionForm"/> クラスのインスタンス。</returns>
         [CreateClassWrapperFromSource]
         public static ScenarioSelectionForm FromSource(object src) => src is null ? null : new ScenarioSelectionForm(src);
+
+        private static FastMethod SaveSettingsMethod;
+        /// <summary>
+        /// <see cref="MainForm.Preferences"/> に設定を保存します。
+        /// </summary>
+        public void SaveSettings() => SaveSettingsMethod.Invoke(Src, null);
     }
 }
