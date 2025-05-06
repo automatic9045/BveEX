cc1e43cc51a02d6fd72b00fddeaccabd2ca598dc
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-05-06 18:00:26 +0900

    feat: [クラスラッパー]自列車の制御段の選択処理を操作可能に

---
+++ Libs/BveTypes/ClassWrappers/Public/VehicleStepper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FastMember;
+using TypeWrapping;
+
+namespace BveTypes.ClassWrappers
+{
+    /// <summary>
+    /// 現在の速度などに応じて、自列車の制御段を選択します。
+    /// </summary>
+    public class VehicleStepper : ClassWrapperBase, ITickable
+    {
+        [InitializeClassWrapper]
+        private static void Initialize(BveTypeSet bveTypes)
+        {
+            ClassMemberSet members = bveTypes.GetClassInfoOf<VehicleStepper>();
+
+            InitializeMethod = members.GetSourceMethodOf(nameof(Initialize));
+            TickMethod = members.GetSourceMethodOf(nameof(Tick));
+        }
+
+        /// <summary>
+        /// オリジナル オブジェクトから <see cref="VehicleStepper"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="src">ラップするオリジナル オブジェクト。</param>
+        protected VehicleStepper(object src) : base(src)
+        {
+        }
+
+        /// <summary>
+        /// オリジナル オブジェクトからラッパーのインスタンスを生成します。
+        /// </summary>
+        /// <param name="src">ラップするオリジナル オブジェクト。</param>
+        /// <returns>オリジナル オブジェクトをラップした <see cref="VehicleStepper"/> クラスのインスタンス。</returns>
+        [CreateClassWrapperFromSource]
+        public static VehicleStepper FromSource(object src) => src is null ? null : new VehicleStepper(src);
+
+        private static FastMethod InitializeMethod;
+        /// <inheritdoc/>
+        public void Initialize() => InitializeMethod.Invoke(Src, null);
+
+        private static FastMethod TickMethod;
+        /// <inheritdoc/>
+        public void Tick(double elapsedSeconds) => TickMethod.Invoke(Src, new object[] { elapsedSeconds });
+
+        /// <summary>
+        /// 毎フレーム呼び出されます。
+        /// </summary>
+        /// <remarks>
+        /// このメソッドはオリジナルではないため、<see cref="ClassMemberSet.GetSourceMethodOf(string, Type[])"/> メソッドから参照することはできません。<br/>
+        /// このメソッドのオリジナルバージョンは <see cref="Tick(double)"/> です。
+        /// </remarks>
+        /// <param name="elapsed">前フレームからの経過時間。</param>
+        /// <seealso cref="Tick(double)"/>
+        public void Tick(TimeSpan elapsed) => Tick(elapsed.TotalSeconds);
+    }
+}
